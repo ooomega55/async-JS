fc7e0bdeeaafc8e581579ba6f6789cc0cb37b25a
ooomega55 <gvf55@outlook.com>
2024-01-18 14:58:10 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var animalContainer = document.getElementById('animal-info')
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+    ourRequest.onload = function() {
+        ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+ourRequest.send();
+});
+
+function renderHTML(data){
+    animalContainer.insertAdjacentHTML('beforeend', 'testing 123');
+}
