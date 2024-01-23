ea244d59991330458bc704999fdead571917835b
ooomega55 <gvf55@outlook.com>
2024-01-22 19:01:49 -0500

    save commit

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+var pageCounter =1
 var animalContainer = document.getElementById('animal-info')
 var btn = document.getElementById('btn');
 
 btn.addEventListener('click', function() {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
     ourRequest.onload = function() {
         ourData = JSON.parse(ourRequest.responseText);
         renderHTML(ourData);
     };
-ourRequest.send();
+    ourRequest.send();
+    pageCounter++;
+    if (pageCounter > 3) {
+        btn.classList.add('hide-me')
+    }
 });
 
 function renderHTML(data){
-    animalContainer.insertAdjacentHTML('beforeend', 'testing 123');
+    var htmlString = '';
+
+    for (i = 0; i < data.length; i++) {
+        htmlString += '<p>' + data[i].name + ' is a ' + data[i].species + ' that likes to eat ';
+
+        for (ii = 0; ii < data[i].foods.likes.length; ii++){
+             if (ii == 0) {
+                htmlString += data[i].foods.likes[ii];
+             } else {
+                htmlString += ' and ' + data[i].foods.likes[ii];
+             }
+        }
+
+        htmlString += '</p>'
+
+    }
+
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
 }
